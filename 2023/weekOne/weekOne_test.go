080d0541b3acb65ddd55a1665c10890f9a97462a
Michael Hewitt <mhewitt@Michaels-MBP.home>
2023-12-02 02:21:01 -0500

    TODO dynamically get input files for test cases

---
+++ 2023/weekOne/weekOne_test.go
@@ -0,0 +1,23 @@
+package adventofcode2023
+
+import (
+	"testing"
+)
+
+func TestWeekOne(t *testing.T) {
+	// Test cases
+	tests := []struct {
+		input  []string
+		output int
+	}{
+		//go:embed  input.txt
+		var inputFile string
+	}
+
+	for _, tt := range tests {
+		result := WeekOne(tt.input)
+		if result != tt.output {
+			t.Errorf("Expected %d, but got %d", tt.output, result)
+		}
+	}
+}
